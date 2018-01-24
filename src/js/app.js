805f80856c5d5fab706c09bccc9358ad3cdd6e67
joshuaward <joshua.ward@me.com>
2018-01-24 16:27:45 -0700

    updating javascript and adding functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,69 @@
-const button = document.querySelector('button');
+const simpleInterest = document.querySelector('.simple-interest');
+const button = document.querySelector('.button');
+const loading = document.querySelector('.loader');
+const results = document.querySelector('.results');
 
-function simpleInterest() {
-	let p = parseInt(document.querySelector('#principal').value);
-	let r = parseInt(document.querySelector('#rate').value);
-	let t = parseInt(document.querySelector('#time').value);
-	let interest = document.querySelector('#interest');
-	let payment = document.querySelector('#payment');
+function calculateResults(e) {
+	// ui elements
+	const principal = document.querySelector('#principal');
+	const rate = document.querySelector('#rate');
+	const time = document.querySelector('#time');
+	const monthlyPayment = document.querySelector('#payment');
+	const totalInterest = document.querySelector('#interest');
+	const totalAmount = document.querySelector('#total');
+	// formula variables
+	const p = parseFloat(principal.value);
+	const r = parseFloat(rate.value);
+	const t = parseFloat(time.value);
 
-	interest.innerHTML = '$' + (p * t * r / 100).toFixed(2);
+	// calculate total interest
+	const interest = (p * t * r / 100);
+	// calculate monthly payment
+	const payment = ((interest + p) / (t * 12)).toFixed(2);
+	// calculate total amount paid
+	const total = (interest + p).toFixed(2);
 
-	let i = interest;
-	payment.innerHTML = '$' + (((p * t * r / 100) + p) / (t * 12)).toFixed(2);
+	if (isFinite(payment)) {
+		totalInterest.innerHTML = '$' + (interest).toFixed(2);
+		monthlyPayment.innerHTML = '$' + payment;
+		totalAmount.innerHTML = '$' + total;
+		// hide loader
+		loading.classList.add('hide');
+		// show results
+		results.classList.remove('hide');
+	} else {
+		// show error
+		showError('Please check your numbers and try again.');
+		// hide loader
+		loading.classList.add('hide');
+	}
 }
 
-button.addEventListener('click', simpleInterest);
+function showError(error) {
+	// create error
+	const errorMessage = document.createElement('div');
+	const calculate = document.querySelector('#calculate');
 
+	errorMessage.className = 'error';
+	errorMessage.appendChild(document.createTextNode(error));
+	simpleInterest.insertBefore(errorMessage, calculate);
+	// clear error
+	setTimeout(clearError, 3000);
+}
+
+function clearError() {
+	// remove error
+	document.querySelector('.error').remove();
+}
+
+button.addEventListener('click', (e) => {
+	console.log('Calculating...');
+	// show loader
+	loading.classList.remove('hide');
+
+	// set timeout
+	setTimeout(calculateResults, 4000);
+
+	// prevent page from reloading on submit
+	e.preventDefault();
+});
